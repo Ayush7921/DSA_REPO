845730f3490763dcc8e1ad11d245fde2af32c439
Ayush7921 <ayush79211@gmail.com>
2024-08-29 20:41:06 +0530

    wheter no. is pos,neg,zero

---
+++ REPO/2nd.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a;
+    cout<<"Enter the number :";
+    cin>>a;
+
+    if(a>0){
+        cout<<"Number is positive ";
+
+    }
+    else {
+         if(a<0){
+        cout<<"Number is negative ";
+         }
+         else{
+            cout<< "number is zero" ;
+         }
+
+    } 
+    
+
+    }
+        
